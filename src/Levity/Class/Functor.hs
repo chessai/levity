2fd322636391f16e853595fb8b7222c4d793fa1c
chessai <chessai1996@gmail.com>
2021-11-13 17:06:02 -0800

    add more FunctorL instances

--- src/Levity/Class/Functor.hs
+++ src/Levity/Class/Functor.hs
@@ -2,30 +2,26 @@ module Levity.Class.Functor
   ( FunctorL(..)
   ) where
 
+import Control.Applicative (ZipList)
 import Control.Exception (Handler)
 import Control.Monad.ST (ST)
 import Data.Complex (Complex)
 import Data.Functor.Const (Const)
 import Data.Functor.Identity (Identity)
 import Data.List.NonEmpty (NonEmpty)
+import Data.Monoid qualified as Monoid
 import Data.Ord (Down)
 import Data.Proxy (Proxy)
+import Data.Semigroup qualified as Semigroup
 import Foreign.Ptr (Ptr)
+import GHC.Base (Type)
 import GHC.Conc (STM)
+import GHC.Generics qualified as Generic
+import GHC.Prim
+import Prelude
 import System.Console.GetOpt (ArgDescr, OptDescr, ArgOrder)
 import Text.ParserCombinators.ReadP (ReadP)
 import Text.ParserCombinators.ReadPrec (ReadPrec)
-
---import GHC.Prim
---import GHC.Types
-
-import GHC.Base (Type)
-import GHC.Prim
-
-import qualified Data.Monoid as Monoid
-import qualified Data.Semigroup as Semigroup
-import qualified GHC.Generics as Generic
-import qualified Prelude
 
 import Levity.Types.Base
 
@@ -36,6 +32,7 @@ class FunctorL (f :: Type -> TYPE r) where
 instance Prelude.Functor f => FunctorL (Base1 f) where
   fmapL = Prelude.fmap
 
+-- Manually-defined instances are because of: https://gitlab.haskell.org/ghc/ghc/-/issues/20680
 deriving via (Base1 []) instance FunctorL []
 deriving via (Base1 Prelude.Maybe) instance FunctorL Prelude.Maybe
 deriving via (Base1 Prelude.IO) instance FunctorL Prelude.IO
@@ -53,7 +50,6 @@ deriving via (Base1 Semigroup.Min) instance FunctorL Semigroup.Min
 deriving via (Base1 Semigroup.Max) instance FunctorL Semigroup.Max
 deriving via (Base1 Semigroup.First) instance FunctorL Semigroup.First
 deriving via (Base1 Semigroup.Last) instance FunctorL Semigroup.Last
-deriving via (Base1 Semigroup.Option) instance FunctorL Semigroup.Option
 deriving via (Base1 ArgOrder) instance FunctorL ArgOrder
 deriving via (Base1 OptDescr) instance FunctorL OptDescr
 deriving via (Base1 ArgDescr) instance FunctorL ArgDescr
@@ -65,52 +61,23 @@ deriving via (Base1 Monoid.Last) instance FunctorL Monoid.Last
 deriving via (Base1 Monoid.Dual) instance FunctorL Monoid.Dual
 deriving via (Base1 Monoid.Sum) instance FunctorL Monoid.Sum
 deriving via (Base1 Monoid.Product) instance FunctorL Monoid.Product
+deriving via (Base1 ZipList) instance FunctorL ZipList
 
-{-
---deriving via (Base1 ZipList) instance FunctorL ZipList
---deriving via (Base1 Generic.U1) instance FunctorL Generic.U1
---deriving via (Base1 Generic.V1) instance FunctorL Generic.V1
---deriving via (Base1 Proxy) instance FunctorL Proxy
---deriving via (Base1 (Generic.Rec1 f)) instance FunctorL (Generic.Rec1 f)
---deriving via (Base1 (Const m)) instance FunctorL (Const m)
-
-deriveFunctor(Semigroup.Arg a)
-deriveFunctorC2(Prelude.Functor f, Functor f, Generic.Rec1 f)
-deriveFunctor(Generic.URec Prelude.Char)
-deriveFunctor(Generic.URec Prelude.Double)
-deriveFunctor(Generic.URec Prelude.Float)
-deriveFunctor(Generic.URec Prelude.Int)
-deriveFunctor(Generic.URec Prelude.Word)
-deriveFunctor(Generic.URec (Ptr ()))
-deriveFunctorC2(Prelude.Functor f, Functor f, Monoid.Alt f)
-#if MIN_VERSION_base(4,12,0)
-deriveFunctorC2(Prelude.Functor f, Functor f, Monoid.Ap f)
-#endif
-deriveFunctor(Const m)
-deriveFunctor((->) r)
-deriveFunctor(Generic.K1 i c)
-deriveFunctorC4(Prelude.Functor f, Prelude.Functor g, Functor f, Functor g, (f Generic.:+: g))
-deriveFunctorC4(Prelude.Functor f, Prelude.Functor g, Functor f, Functor g, (f Generic.:*: g))
-instance Functor ((# , #) a) where
-  fmap f (# a, b #) = (# a, f b #)
-
-instance Functor ((# ,, #) a b) where
-  fmap f (# a, b, c #) = (# a, b, f c #)
-
-#define deriveFunctor(ty)     \
-instance Functor (ty) where { \
-    fmap = Prelude.fmap       \
-}
-
-#define deriveFunctorC2(c1,c2,ty)         \
-instance (c1, c2) => Functor (ty) where { \
-    fmap = Prelude.fmap                   \
-}
-
-#define deriveFunctorC4(c1,c2,c3,c4,ty)           \
-instance (c1, c2, c3, c4) => Functor (ty) where { \
-    fmap = Prelude.fmap                           \
-}
-
-
--}
+deriving via (Base1 Generic.U1) instance FunctorL Generic.U1
+deriving via (Base1 Generic.V1) instance FunctorL Generic.V1
+instance (Prelude.Functor f) => FunctorL (Generic.Rec1 f) where { fmapL = Prelude.fmap }
+deriving via (Base1 (Generic.URec (Ptr ()))) instance FunctorL (Generic.URec (Ptr ()))
+deriving via (Base1 (Generic.URec Char)) instance FunctorL (Generic.URec Char)
+deriving via (Base1 (Generic.URec Double)) instance FunctorL (Generic.URec Double)
+deriving via (Base1 (Generic.URec Float)) instance FunctorL (Generic.URec Float)
+deriving via (Base1 (Generic.URec Int)) instance FunctorL (Generic.URec Int)
+deriving via (Base1 (Generic.K1 i c)) instance FunctorL (Generic.K1 i c)
+deriving via (Base1 Proxy) instance FunctorL Proxy
+deriving via (Base1 (Const m)) instance FunctorL (Const m)
+instance (Functor f) => FunctorL (Monoid.Alt f) where { fmapL = fmap }
+instance (Functor f) => FunctorL (Monoid.Ap f) where { fmapL = fmap }
+deriving via (Base1 ((->) r)) instance FunctorL ((->) r)
+instance (Functor f, Functor g) => FunctorL (f Generic.:+: g) where { fmapL = fmap }
+instance (Functor f, Functor g) => FunctorL (f Generic.:*: g) where { fmapL = fmap }
+instance FunctorL ((# , #) a) where { fmapL f (# a, b #) = (# a, f b #) }
+instance FunctorL ((# ,, #) a b) where { fmapL f (# a, b, c #) = (# a, b, f c #) }
