0e6cb8cba918ea08162466877c00a27df7cd7c85
chessai <chessai1996@gmail.com>
2021-11-23 15:55:00 -0600

    implement IO

--- src/Levity/Types/IO.hs
+++ src/Levity/Types/IO.hs
@@ -1,3 +1,19 @@
 module Levity.Types.IO
-  (
+  ( IO(..)
   ) where
+
+import "base" GHC.Exts
+import "base" Data.Kind (Type)
+import "base" Prelude qualified
+
+import Levity.Types.ST qualified as STL
+import Levity.Class.Functor
+import Levity.Class.Applicative
+import Levity.Class.Monad
+
+type IO :: TYPE r -> Type
+newtype IO a = IO (STL.ST RealWorld a)
+  deriving newtype (Prelude.Functor, Prelude.Applicative, Prelude.Monad)
+  deriving newtype (FunctorL, ApplyL, ApplicativeL, MonadL)
+
+type role IO representational
